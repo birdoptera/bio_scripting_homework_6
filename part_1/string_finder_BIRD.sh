807cfa426111a693208fbadf938bffc9e02e41d9
Gwendolyn <birdoptera@gmail.com>
2019-03-04 09:45:26 -0600

    Cleaned up script and added comments

--- part_1/string_finder_BIRD.sh
+++ part_1/string_finder_BIRD.sh
@@ -2,22 +2,17 @@
 
 #searches for a specific word in a file and
 #displays how many times and on what line numbers of the file
-#they were found.
+#they were found. Argument 1 is the file given and argument 2 is the word chosen
 
+#assign names to arguments
 file="$1"
 word="$2"
+#set up output message
 echo "The word $2 was found in this number of lines:"
+#count the number of lines in which the word appears
 grep -o $word $file |grep -c $word $file
+#set up the output message
 echo "The word $2 was found in these lines:"
+#count the number of lines in which the word appears
 grep -n $word $file | cut -f1 -d: | sort -u
-#-n flag output in grep and use with cut
-
-#write the script, taking a filename as the first argument
-#and the word being searched as the second argument
-
-#the script should display a summary message showing
-#a) how many matches were found at least once per line
-#b) the line numbers where the string occured at least once per line
-#c) the file name being searched
-
 
