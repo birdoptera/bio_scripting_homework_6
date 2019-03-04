db3fcdf02a9fd3b289547c37df31e8c3591071c5
Gwendolyn <birdoptera@gmail.com>
2019-03-04 09:16:20 -0600

    Wrote first draft of new file, assigned arguments and found how to print line numbers. Output message needs to be written and count only counts lines, not instances

---
+++ part_1/string_finder_BIRD.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#searches for a specific word in a file and
+#displays how many times and on what line numbers of the file
+#they were found.
+
+file="$1"
+word="$2"
+
+grep -o $word $file |grep -c $word $file
+grep -n $word $file
+#-n flag output in grep and use with cut
+
+#write the script, taking a filename as the first argument
+#and the word being searched as the second argument
+
+#the script should display a summary message showing
+#a) how many matches were found at least once per line
+#b) the line numbers where the string occured at least once per line
+#c) the file name being searched
+
+
